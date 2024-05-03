9dbc7a2a167210b48619d3bd6b83899a8f9acb58
ChanMor <morelos.franzchristian@gmail.com>
2024-05-03 17:16:53 +0800

    Initial commit

---
+++ api/controllers/auth.js
@@ -0,0 +1,11 @@
+import model from 'mongoose';
+
+const register = (req, res) => {
+    // Implement user registration logic here
+};
+
+const login = (req, res) => {
+    // Implement user login logic here
+};
+
+export { register, login }
---
+++ api/models/model.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const User = mongoose.model('User', {
+    firstName: String,
+    middleName: String,
+    lastName: String,
+    userType: String,
+    email: String,
+    password: String
+}, 'users');
+
+const Product = mongoose.model('Product', {
+    productId: String,
+    productName: String,
+    productDescription: String,
+    productType: Number,
+    productQuantity: Number
+}, 'products');
+
+
+const OrderTransaction = mongoose.model('OrderTransaction', {
+    transactionId: String,
+    productId: String,
+    orderQuantity: Number,
+    orderStatus: Number,
+    email: String,
+    dateOrdered: Date,
+    time: String
+}, 'orderTransactions');
+
+export { User, Product, OrderTransaction };
