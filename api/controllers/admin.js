1c06b2b3e31e2c058bc183bd5996c1d2242b5e05
ChanMor <morelos.franzchristian@gmail.com>
2024-05-07 22:51:32 +0800

    code: added fulfill order function

--- api/controllers/admin.js
+++ api/controllers/admin.js
@@ -1,4 +1,4 @@
-import { OrderTransaction } from "../models/model.js";
+import { User, Product, OrderTransaction } from "../models/model.js";
 
 const getSales = (req, res) => {
     // Implement user login logic here
@@ -16,8 +16,40 @@ const addProduct = (req, res) => {
     // Implement user login logic here
 };
 
-const fulfillOrder = (req, res) => {
-    // Implement user login logic here
+const fulfillOrder = async (req, res) => {
+    try {
+        const { transactionId } = req.body;
+
+        // Find the transaction using the transaction ID
+        const transaction = await OrderTransaction.findOne({transactionId: transactionId, orderStatus: 0});
+        
+        if (!transaction) {
+            return res.status(404).send('Transaction not found or is completed or cancelled.');
+        }
+
+        transaction.orderStatus = 1;
+        await transaction.save();
+
+        // Find the product associated with this transaction
+        const product = await Product.findOne({productId: transaction.productId});
+        
+        let productQuantity = product.productQuantity;
+        const orderQuantity = transaction.orderQuantity;
+
+        // dectement product quantity if has enough
+        if (productQuantity >= orderQuantity) {
+
+            product.productQuantity -= transaction.orderQuantity;
+            await product.save();
+
+            res.status(200).send('Order has been fulfilled and product quantity updated.');
+            return 
+        }
+
+        res.status(400).send('Not enough stock to fulfill the order.');
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 };
 
 export { getSales, getUsers, getOrders, addProduct, fulfillOrder };
--- api/request.js
+++ api/request.js
@@ -1,16 +1,23 @@
 import needle from 'needle';
 
-const newUser = {
-    firstName: 'John',
-    lastName: 'Doe',
-    userType: 'customer',
-    email: 'john@example.com',
-    password: 'password123'
-};
+// const newUser = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     userType: 'customer',
+//     email: 'john@example.com',
+//     password: 'password123'
+// };
 
+// needle.post('http://localhost:3000/auth/register', newUser, 
+// (err, res) => {
+//     console.log('Response:', res.body);
+// });
 
+const fulfillOrder = {
+    transactionId: 'T001'
+}
 
-needle.post('http://localhost:3000/auth/register', newUser, 
+needle.post('http://localhost:3000/admin/order-fulfillment', fulfillOrder,
 (err, res) => {
-    console.log('Response:', res.body);
+    console.log('Response', res.body);
 });
