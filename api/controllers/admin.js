27539f109c7fd66b3c1426373c08d78fe988d0ea
Axel Balitaan <balitaanaxel@gmail.com>
2024-05-07 16:44:51 +0800

    feat: sales of each item now categorized according to order status

--- api/controllers/admin.js
+++ api/controllers/admin.js
@@ -2,25 +2,36 @@ import { User, Product, OrderTransaction } from "../models/model.js";
 
 const getSales = async (req, res) => {
     try {
+        // Gets order transactions and product list
         const orderTransactionList = await OrderTransaction.find();
-        const productList = await Product.find();
+        const productList = await Product.find();     
 
-        const productSales = {};
+        const productSales = {};    // Initialize empty object {productId: {status: quantity}}
 
-        for (const product of productList) {
-            const productId = product.productId;
-            const totalSales = orderTransactionList
-                .filter(order => order.productId === productId)
-                .reduce((sales, order) => sales + order.orderQuantity, 0);
-            productSales[productId] = totalSales;
+        for (const product of productList) {    // For every product in productList
+            const productId = product.productId;  // Gets productId
+
+            // Initializes an object to store quantity by status, with default values as 0
+            const salesByStatus = { 0: 0, 1: 0, 2: 0 }; 
+
+            // Computes total sales and categorizes by status
+            orderTransactionList.filter(order => order.productId === productId)
+                .forEach(order => {
+                    salesByStatus[order.orderStatus] += order.orderQuantity;
+                });
+
+            productSales[productId] = salesByStatus; // Adds to the object {productId: {status: quantity}}
         }
 
-        res.json(productSales);
+        res.json(productSales);     // Returns productSales
+
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal server error' });
     }
 };
+
+
 
 const getUsers = async (req, res) => {
     res.json(await User.find());
