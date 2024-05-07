4d0b7e5aca2d3f7edee31c9798da9a10b93f5646
Axel Balitaan <balitaanaxel@gmail.com>
2024-05-07 16:22:37 +0800

    code: added endpoint that generate a json of sales of each items

--- api/controllers/admin.js
+++ api/controllers/admin.js
@@ -1,9 +1,25 @@
-import { OrderTransaction } from "../models/model.js";
+import { User, Product, OrderTransaction } from "../models/model.js";
 
 const getSales = async (req, res) => {
-    // Get Orders
-    // Get Products
-    // Join Orders and Products
+    try {
+        const orderTransactionList = await OrderTransaction.find();
+        const productList = await Product.find();
+
+        const productSales = {};
+
+        for (const product of productList) {
+            const productId = product.productId;
+            const totalSales = orderTransactionList
+                .filter(order => order.productId === productId)
+                .reduce((sales, order) => sales + order.orderQuantity, 0);
+            productSales[productId] = totalSales;
+        }
+
+        res.json(productSales);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 };
 
 const getUsers = async (req, res) => {
--- api/request.js
+++ api/request.js
@@ -14,3 +14,7 @@ needle.post('http://localhost:3000/auth/register', newUser,
 (err, res) => {
     console.log('Response:', res.body);
 });
+
+needle.get('http://localhost:3000/admin/sales', (err, res) => {
+    console.log(res.body);
+});
