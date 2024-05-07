32097714cca6fb301651cc7f957692ca0790888a
michaelcariaso <mdcariaso@up.edu.ph>
2024-05-07 23:04:52 +0800

    feat: Created initial AddProduct page for taking form inputs and adding a new product

--- api/request.js
+++ api/request.js
@@ -1,16 +1,29 @@
-import needle from 'needle';
+import needle from "needle";
 
 const newUser = {
-    firstName: 'John',
-    lastName: 'Doe',
-    userType: 'customer',
-    email: 'john@example.com',
-    password: 'password123'
+  firstName: "John",
+  lastName: "Doe",
+  userType: "customer",
+  email: "john@example.com",
+  password: "password123",
 };
 
+const newProduct = {
+  productId: "131313131",
+  productName: "Apple",
+  productDescription: "Sweet",
+  productType: 2,
+  productQuantity: 5,
+};
 
-
-needle.post('http://localhost:3000/auth/register', newUser, 
-(err, res) => {
-    console.log('Response:', res.body);
+needle.post("http://localhost:3000/auth/register", newUser, (err, res) => {
+  console.log("Response:", res.body);
 });
+
+needle.post(
+  "http://localhost:3000/admin/add-products",
+  newProduct,
+  (err, res) => {
+    console.log(res.body);
+  }
+);
