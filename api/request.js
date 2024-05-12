551adec0b75f0055361a5062b09359ed3309bdf6
ChanMor <morelos.franzchristian@gmail.com>
2024-05-12 22:35:24 +0800

    feat: added get orders function

--- api/request.js
+++ api/request.js
@@ -1,34 +1,34 @@
 import needle from "needle";
 
 
-const newUser = {
-  firstName: "John",
-  lastName: "Doe",
-  userType: "customer",
-  email: "john@example.com",
-  password: "password123",
-};
+// const newUser = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   userType: "customer",
+//   email: "john@example.com",
+//   password: "password123",
+// };
 
-//sample product
-const newProduct = {
-  productId: "525242",
-  productName: "Apple",
-  productDescription: "Sweet",
-  productType: 2,
-  productQuantity: 5,
-};
+// //sample product
+// const newProduct = {
+//   productId: "525242",
+//   productName: "Apple",
+//   productDescription: "Sweet",
+//   productType: 2,
+//   productQuantity: 5,
+// };
 
-needle.post("http://localhost:3000/auth/register", newUser, (err, res) => {
-  console.log("Response:", res.body);
-});
+// needle.post("http://localhost:3000/auth/register", newUser, (err, res) => {
+//   console.log("Response:", res.body);
+// });
 
-needle.post(
-  "http://localhost:3000/admin/add-products",
-  newProduct,
-  (err, res) => {
-    console.log(res.body);
-  }
-);
+// needle.post(
+//   "http://localhost:3000/admin/add-products",
+//   newProduct,
+//   (err, res) => {
+//     console.log(res.body);
+//   }
+// );
 
 // const newUser = {
 //     firstName: 'John',
@@ -97,3 +97,10 @@ needle.post(
 // (err, res) => {
 //     console.log('Response', res.body);
 // });
+
+const email = 'john.doe@example.com';
+
+needle.get(`http://localhost:3000/user/orders?email=${email}`,
+(err, res) => {
+    console.log('Response', res.body);
+});
