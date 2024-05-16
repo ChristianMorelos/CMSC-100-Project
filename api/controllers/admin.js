b61f79203440b613894fcab731ec12b506f0c2ed
michaelcariaso <mdcariaso@up.edu.ph>
2024-05-16 16:41:40 +0800

    chore: Made the productId a random 8 character string using uuidv4

--- api/controllers/admin.js
+++ api/controllers/admin.js
@@ -1,40 +1,40 @@
 import { User, Product, OrderTransaction } from "../models/model.js";
+import { v4 as uuidv4 } from "uuid";
 
 const getSales = async (req, res) => {
-    try {
-        // Gets order transactions and product list
-        const orderTransactionList = await OrderTransaction.find();
-        const productList = await Product.find();     
+  try {
+    // Gets order transactions and product list
+    const orderTransactionList = await OrderTransaction.find();
+    const productList = await Product.find();
 
-        const productSales = {};    // Initialize empty object {productId: {status: quantity}}
+    const productSales = {}; // Initialize empty object {productId: {status: quantity}}
 
-        for (const product of productList) {    // For every product in productList
-            const productId = product.productId;  // Gets productId
+    for (const product of productList) {
+      // For every product in productList
+      const productId = product.productId; // Gets productId
 
-            // Initializes an object to store quantity by status, with default values as 0
-            const salesByStatus = { 0: 0, 1: 0, 2: 0 }; 
+      // Initializes an object to store quantity by status, with default values as 0
+      const salesByStatus = { 0: 0, 1: 0, 2: 0 };
 
-            // Computes total sales and categorizes by status
-            orderTransactionList.filter(order => order.productId === productId)
-                .forEach(order => {
-                    salesByStatus[order.orderStatus] += order.orderQuantity;
-                });
+      // Computes total sales and categorizes by status
+      orderTransactionList
+        .filter((order) => order.productId === productId)
+        .forEach((order) => {
+          salesByStatus[order.orderStatus] += order.orderQuantity;
+        });
 
-            productSales[productId] = salesByStatus; // Adds to the object {productId: {status: quantity}}
-        }
-
-        res.json(productSales);     // Returns productSales
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal server error' });
+      productSales[productId] = salesByStatus; // Adds to the object {productId: {status: quantity}}
     }
+
+    res.json(productSales); // Returns productSales
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
-
-
 const getUsers = async (req, res) => {
-    res.json(await User.find());
+  res.json(await User.find());
 };
 
 //get orders method
@@ -45,10 +45,15 @@ const getOrders = async (req, res) => {
 
 //add product method
 const addProduct = async (req, res) => {
+  //random product id
+  var productId = uuidv4();
+  //split the random alphanumeric by "-"
+  productId = productId.split("-");
+  //get the first 8 character string of the random id
+  productId = productId[0];
 
   //check if the product fields are complete
   if (
-    req.body.productId &&
     req.body.productName &&
     req.body.productDescription &&
     req.body.productType &&
@@ -56,7 +61,13 @@ const addProduct = async (req, res) => {
   ) {
     try {
       //create new instance of product with the given inputs
-      const newProduct = new Product(req.body);
+      const newProduct = new Product({
+        productId: productId,
+        productName: req.body.productName,
+        productDescription: req.body.productDescription,
+        productType: req.body.productType,
+        productQuantity: req.body.productQuantity,
+      });
       //save product to the database
       await newProduct.save();
 
@@ -73,39 +84,45 @@ const addProduct = async (req, res) => {
 };
 
 const fulfillOrder = async (req, res) => {
-    try {
-        const { transactionId } = req.body;
+  try {
+    const { transactionId } = req.body;
 
-        // Find the transaction using the transaction ID
-        const transaction = await OrderTransaction.findOne({transactionId: transactionId, orderStatus: 0});
-        
-        if (!transaction) {
-            return res.status(404).send('Transaction not found or is completed or cancelled.');
-        }
+    // Find the transaction using the transaction ID
+    const transaction = await OrderTransaction.findOne({
+      transactionId: transactionId,
+      orderStatus: 0,
+    });
 
-        transaction.orderStatus = 1;
-        await transaction.save();
-
-        // Find the product associated with this transaction
-        const product = await Product.findOne({productId: transaction.productId});
-        
-        let productQuantity = product.productQuantity;
-        const orderQuantity = transaction.orderQuantity;
-
-        // dectement product quantity if has enough
-        if (productQuantity >= orderQuantity) {
-
-            product.productQuantity -= transaction.orderQuantity;
-            await product.save();
-
-            res.status(200).send('Order has been fulfilled and product quantity updated.');
-            return 
-        }
-
-        res.status(400).send('Not enough stock to fulfill the order.');
-    } catch (error) {
-        res.status(500).send(error.message);
+    if (!transaction) {
+      return res
+        .status(404)
+        .send("Transaction not found or is completed or cancelled.");
     }
+
+    transaction.orderStatus = 1;
+    await transaction.save();
+
+    // Find the product associated with this transaction
+    const product = await Product.findOne({ productId: transaction.productId });
+
+    let productQuantity = product.productQuantity;
+    const orderQuantity = transaction.orderQuantity;
+
+    // dectement product quantity if has enough
+    if (productQuantity >= orderQuantity) {
+      product.productQuantity -= transaction.orderQuantity;
+      await product.save();
+
+      res
+        .status(200)
+        .send("Order has been fulfilled and product quantity updated.");
+      return;
+    }
+
+    res.status(400).send("Not enough stock to fulfill the order.");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
 export { getSales, getUsers, getOrders, addProduct, fulfillOrder };
